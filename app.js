1b40a1639e1157b098f3e4097dfd37e888ff601f
Eric Patrick <erstpa@gmail.com>
2016-06-02 15:39:53 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var Products = {}; 
+
+Products.regularBracelet = [];
+Products.lightBracelet = [];
+Products.wideBracelet = [];
+Products.keyFob = [];
+Products.gearTag = [];
+Products.neckID = [];
+Products.necklace = [];
+Products.neckCord = [];
