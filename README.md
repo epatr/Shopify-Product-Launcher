48c4dda072d0686d24f1c71c28631ab3392e8c58
Eric Patrick <erstpa@gmail.com>
2016-06-06 11:56:05 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Shopify Product Launcher
+### Customizable tool to help create collections of products
+
+Shopify Product Launcher is a JavaScript application that aids in creating
+complete collections of products in Shopify. This is useful if you have a 
+product lineup that you need to create an alternate version of. For example,
+You sell bracelets, necklaces, and key fobs and want to duplicate them 
+with a new logo. 
+
+### Usage
+
+Simply edit the `products.json` JSON file with your line of products. After
+that, fire up `index.html` in your browser and follow the steps. 
--- app.js
+++ app.js
@@ -1,10 +1,49 @@
-var Products = {}; 
+var Collection = function() {
 
-Products.regularBracelet = [];
-Products.lightBracelet = [];
-Products.wideBracelet = [];
-Products.keyFob = [];
-Products.gearTag = [];
-Products.neckID = [];
-Products.necklace = [];
-Products.neckCord = [];
+  var products = [ {
+      "title": "Regular Survival Bracelet™",
+      "price": "29.99",
+      "tags": "",
+      "description": ""
+    }, {
+      "title": "Light Duty Survival Bracelet™",
+      "price": "29.99",
+      "tags": "",
+      "description": ""
+    }, {
+      "title": "Wide Survival Bracelet™",
+      "price": "32.99",
+      "tags": "",
+      "description": ""
+    }, {
+      "title": "Key Fob",
+      "price": "19.95",
+      "tags": "",
+      "description": ""
+    }, {
+      "title": "Gear Tag",
+      "price": "12.95",
+      "tags": "",
+      "description": ""
+    }, {
+      "title": "Neck ID Lanyard",
+      "price": "21.95",
+      "tags": "",
+      "description": ""
+    }, {
+      "title": "Necklace",
+      "price": "29.95",
+      "tags": "",
+      "description": ""
+    }, {
+      "title": "Neck Cord",
+      "price": "14.95",
+      "tags": "",
+      "description": ""
+    }
+  ];
+};
+
+var convertURL = function(text) {
+  return text.toLowerCase().replace(/ /g,'-').replace(/[^\w-]+/g,'');
+};
